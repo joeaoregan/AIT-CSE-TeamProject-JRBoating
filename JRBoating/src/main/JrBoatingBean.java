672a996cefa850e50aa4a7c08bb154c5e7edb116
A00258344 <A00258344@w301-E26.studentnet.ait.ie>
2018-08-14 14:09:31 +0100

    Sorcha update to boat bean and test

--- JRBoating/src/main/JrBoatingBean.java
+++ JRBoating/src/main/JrBoatingBean.java
@@ -13,10 +13,20 @@ public class JrBoatingBean {
 	
 	public void addBoat(Boat boat) {
 		boats.add(boat);
+		
 	}
 	public void removeBoat(Boat boat) {
 		boats.remove(boat);
 	}
+	public int boatCount() {
+		return boats.size();
+	}
+	/*public String viewBoat () {
+		StringBuilder string = new StringBuilder();
+		for (Boat boat : boats) {
+			string.append(boats.toString());	
+		}	
+	}*/
 	
 	
 }
