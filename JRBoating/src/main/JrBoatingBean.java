91865a92b300a99b07c87cea1c5c29990d1b2ab4
A00258344 <A00258344@w301-E26.studentnet.ait.ie>
2018-08-14 16:03:21 +0100

    sorcha update to boatCount method added to JRBoatingBean

--- JRBoating/src/main/JrBoatingBean.java
+++ JRBoating/src/main/JrBoatingBean.java
@@ -1,37 +1,4 @@
-<<<<<<< HEAD
-package main;
 
-import java.util.ArrayList;
-
-public class JrBoatingBean {
-
-	ArrayList<Boat> boats;
-	
-	
-	public JrBoatingBean() {
-		boats = new ArrayList<Boat> ();
-	}
-	
-	public void addBoat(Boat boat) {
-		boats.add(boat);
-		
-	}
-	public void removeBoat(Boat boat) {
-		boats.remove(boat);
-	}
-	public int boatCount() {
-		return boats.size();
-	}
-	/*public String viewBoat () {
-		StringBuilder string = new StringBuilder();
-		for (Boat boat : boats) {
-			string.append(boats.toString());	
-		}	
-	}*/
-	
-	
-}
-=======
 package main;
 
 import java.util.ArrayList;
@@ -52,6 +19,9 @@ public class JrBoatingBean {
 
 	public void removeBoat(Boat boat) {
 		boats.remove(boat);
+	}
+	public int boatCount() {
+		return boats.size();
 	}
 
 	public boolean checkUniqueUsername(String username) {
@@ -82,4 +52,3 @@ public class JrBoatingBean {
 	}
 
 }
->>>>>>> branch 'master' of https://sorchabruton@bitbucket.org/boating/jrboating.git
