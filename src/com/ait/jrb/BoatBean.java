be287e284e1a6c091cbb2f799e4d3babb3a71883
unknown <A00258344@w301-E26.studentnet.ait.ie>
2018-08-17 11:45:35 +0100

    SB & JOR: Tidy up footer

--- src/com/ait/jrb/BoatBean.java
+++ src/com/ait/jrb/BoatBean.java
@@ -3,7 +3,7 @@ package com.ait.jrb;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
+//import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
