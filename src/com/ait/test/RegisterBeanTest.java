81b1e1f393eb1435364fc67f1cf3af1ccc31165f
Elaine Ottero <a00248619@student.ait.ie>
2018-08-21 16:39:38 +0100

    RegisterBeanTest

--- src/com/ait/test/RegisterBeanTest.java
+++ src/com/ait/test/RegisterBeanTest.java
@@ -14,39 +14,57 @@ class RegisterBeanTest {
 	void setUp() {
 		registerBean = new RegisterBean();
 	}
+
 	@Test
 	void testChangeFirstName() {
 		registerBean.setFirstName("Sorcha");
 		assertEquals("Sorcha", registerBean.getFirstName());
 	}
+
 	@Test
 	void testChangeUsername() {
 		registerBean.setUsername("SorchaB");
 		assertEquals("SorchaB", registerBean.getUsername());
 	}
+
 	@Test
 	void testChangeLastName() {
 		registerBean.setLastName("Bruton");
 		assertEquals("Bruton", registerBean.getLastName());
 	}
+
 	@Test
 	void testChangePassword() {
 		registerBean.setPassword("pword");
 		assertEquals("pword", registerBean.getPassword());
 	}
+
 	@Test
 	void testChangeAddress() {
 		registerBean.setAddress("Athlone");
 		assertEquals("Athlone", registerBean.getAddress());
 	}
+
 	@Test
 	void testChangePhone() {
 		registerBean.setPhone("0871234567");
 		assertEquals("0871234567", registerBean.getPhone());
 	}
+
 	@Test
 	void testChangeType() {
 		registerBean.setType(2);
 		assertEquals(2, registerBean.getType());
+	}
+	
+	@Test
+	void testGetObscuredPassord() {
+		registerBean.setPassword("Test");
+		assertEquals("T...t", registerBean.getObscuredPassword());
+	}
+
+	@Test
+	void testRegister2() {
+		assertEquals("/customer/RegistrationVerify.xhtml", registerBean.register2());
 	}
 }
