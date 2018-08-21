379b880a81ca284cc903a69d9f04f370c2331d11
A00258344 <A00258344@w301-E26.studentnet.ait.ie>
2018-08-21 14:12:19 +0100

    OrderBean and OrderBeanTest SB

--- src/com/ait/jrb/OrderBean.java
+++ src/com/ait/jrb/OrderBean.java
@@ -2,7 +2,6 @@ package com.ait.jrb;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
