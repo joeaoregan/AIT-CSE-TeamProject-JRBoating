c297ca8617b71d84aa50a4f3661b9f39ca77cdb1
Joe O'Regan <joeaoregan@yahoo.ie>
2018-08-22 16:17:53 +0100

    Pricing Bean test, changed default values to 1 for both inputs

--- src/com/ait/test/PricingBeanTest.java
+++ src/com/ait/test/PricingBeanTest.java
@@ -20,26 +20,20 @@ class PricingBeanTest {
 	}
 
 	@Test
-<<<<<<< HEAD
 	void testConstructor() {
-		assertEquals(0, pricingBean.getAmount());
+		assertEquals(1, pricingBean.getAmount());
 		assertEquals(Double.valueOf(0.0), pricingBean.getPrice());
-		assertEquals(0, pricingBean.getPercent());
+		assertEquals(1, pricingBean.getPercent());
 	}
 
 	@Test
-=======
->>>>>>> 6ed44d560def34af08b8d9dd96ce2c8fa48e7d83
 	void testSetters() {
 		pricingBean.setAmount(10);
 		assertEquals(10, pricingBean.getAmount());
 		pricingBean.setPrice(1.25);
 		assertEquals(Double.valueOf(1.25), pricingBean.getPrice());
-<<<<<<< HEAD
 		pricingBean.setPercent(5);
 		assertEquals(5, pricingBean.getPercent());	
-=======
->>>>>>> 6ed44d560def34af08b8d9dd96ce2c8fa48e7d83
 		pricingBean.setName("test");
 		assertEquals("test", pricingBean.getName());
 		ArrayList<PricingStructure> prices = new ArrayList<PricingStructure>();
@@ -54,24 +48,18 @@ class PricingBeanTest {
 	@Test
 	void testRenderTableEmpty() {
 		assertEquals(false, pricingBean.renderTable());	// String not boolean
-<<<<<<< HEAD
 		pricingBean.addPricing("test",  10,  20);
 		assertEquals(true, pricingBean.renderTable());
-=======
 		//ArrayList prices= new ArrayList<PricingStructure>();
 		//PricingStructure price= new PricingStructure("type", 10, 1);
 		//prices.add(price);
 		//assertEquals(1, prices.size());
 		//assertEquals(true, pricingBean.renderTable());
->>>>>>> 6ed44d560def34af08b8d9dd96ce2c8fa48e7d83
 	}
 	//test False version of SaveAction?
 	@Test
 	void testSaveAction() {
-<<<<<<< HEAD
 		pricingBean.addPricing("test",  10,  20);
-=======
->>>>>>> 6ed44d560def34af08b8d9dd96ce2c8fa48e7d83
 		assertEquals(null, pricingBean.saveAction());
 		//ArrayList prices= new ArrayList<PricingStructure>();
 		//PricingStructure price= new PricingStructure("type", 10, 1);
@@ -81,31 +69,19 @@ class PricingBeanTest {
 	
 	@Test
 	void testEditPricingStructure() {
-<<<<<<< HEAD
 		PricingStructure pricingStructure = new PricingStructure("item2", 10, 10);
-=======
-		PricingStructure pricingStructure = new PricingStructure("item2", 10, 1);
->>>>>>> 6ed44d560def34af08b8d9dd96ce2c8fa48e7d83
 		assertEquals(null, pricingBean.editPricingStructure(pricingStructure));
 	}
 	
 	@Test
 	void testDeletePricingStructure() {
-<<<<<<< HEAD
 		PricingStructure pricingStructure = new PricingStructure("item2", 10, 10);
-=======
-		PricingStructure pricingStructure = new PricingStructure("item2", 10, 1);
->>>>>>> 6ed44d560def34af08b8d9dd96ce2c8fa48e7d83
 		assertEquals(null, pricingBean.deletePricingStructure(pricingStructure));		
 	}
 	
 	@Test
 	void testAddPricing() {
-<<<<<<< HEAD
 		pricingBean.addPricing("test",  10,  20);
-=======
-		pricingBean.addPricing("type", 10, 1);
->>>>>>> 6ed44d560def34af08b8d9dd96ce2c8fa48e7d83
 		assertEquals(1, pricingBean.getPrices().size());
 	}
 
